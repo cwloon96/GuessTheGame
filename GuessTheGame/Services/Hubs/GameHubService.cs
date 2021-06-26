e969ed9375400a5a1c6a31e09cd47809df191f17
Woei Loon <woeiloon96@gmail.com>
2021-06-26 18:58:29 +0800

    remove unused async await keyword

--- GuessTheGame/Services/Hubs/GameHubService.cs
+++ GuessTheGame/Services/Hubs/GameHubService.cs
@@ -14,35 +14,23 @@ namespace GuessTheGame.Services.Hubs
             _hubContext = hubContext;
         }
 
-        public async Task AddToGroupAsync(Guid roomGuid, string connectionId)
-        {
-            await _hubContext.Groups.AddToGroupAsync(connectionId, roomGuid.ToString());
-        }
+        public Task AddToGroupAsync(Guid roomGuid, string connectionId) =>
+            _hubContext.Groups.AddToGroupAsync(connectionId, roomGuid.ToString());
 
-        public async Task RemoveFromGroupAsync(Guid roomGuid, string connectionId)
-        {
-            await _hubContext.Groups.RemoveFromGroupAsync(connectionId, roomGuid.ToString());
-        }
+        public Task RemoveFromGroupAsync(Guid roomGuid, string connectionId) =>
+            _hubContext.Groups.RemoveFromGroupAsync(connectionId, roomGuid.ToString());
 
-        public async Task SendToGroupAsync(Guid roomGuid, string method, object obj)
-        {
-            await _hubContext.Clients.Group(roomGuid.ToString()).SendAsync(method, obj);
-        }
+        public Task SendToGroupAsync(Guid roomGuid, string method, object obj) =>
+            _hubContext.Clients.Group(roomGuid.ToString()).SendAsync(method, obj);
 
-        public async Task UpdateCurrentWordAsync(string maskedWord, string connectionId)
-        {
-            await _hubContext.Clients.Client(connectionId).SendAsync("RefreshWord", maskedWord);
-        }
+        public Task UpdateCurrentWordAsync(string maskedWord, string connectionId) =>
+            _hubContext.Clients.Client(connectionId).SendAsync("RefreshWord", maskedWord);
 
-        public async Task RefreshWordAsync(Guid roomGuid, string word)
-        {
-            await _hubContext.Clients.Group(roomGuid.ToString()).SendAsync("RefreshWord", word);
-        }
+        public Task RefreshWordAsync(Guid roomGuid, string word) =>
+            _hubContext.Clients.Group(roomGuid.ToString()).SendAsync("RefreshWord", word);
 
-        public async Task PlayerJoinAsync(Guid roomGuid, object obj)
-        {
-            await _hubContext.Clients.Group(roomGuid.ToString()).SendAsync("PlayerJoined", obj);
-        }
+        public Task PlayerJoinAsync(Guid roomGuid, object obj) =>
+            _hubContext.Clients.Group(roomGuid.ToString()).SendAsync("PlayerJoined", obj);
 
         public async Task PlayerLeftAsync(Guid roomGuid, string connectionId, string playerUsername)
         {
@@ -52,19 +40,13 @@ namespace GuessTheGame.Services.Hubs
             await RefreshWordAsync(roomGuid, string.Empty);
         }
 
-        public async Task UpdateBalanceAsync(Guid roomGuid, string username, int money)
-        {
-            await _hubContext.Clients.Group(roomGuid.ToString()).SendAsync("UpdatePlayer", username, money);
-        }
+        public Task UpdateBalanceAsync(Guid roomGuid, string username, int money) =>
+            _hubContext.Clients.Group(roomGuid.ToString()).SendAsync("UpdatePlayer", username, money);
 
-        public async Task ReceiveAnswerAsync(Guid roomGuid, string username, string answer, bool correct)
-        {
-            await _hubContext.Clients.Group(roomGuid.ToString()).SendAsync("ReceiveAnswer", username, answer, correct);
-        }
+        public Task ReceiveAnswerAsync(Guid roomGuid, string username, string answer, bool correct) =>
+            _hubContext.Clients.Group(roomGuid.ToString()).SendAsync("ReceiveAnswer", username, answer, correct);
 
-        public async Task PopulatePlayersAsync(string connectionId, object obj)
-        {
-            await _hubContext.Clients.Client(connectionId).SendAsync("PopulatePlayers", obj);
-        }
+        public Task PopulatePlayersAsync(string connectionId, object obj) =>
+            _hubContext.Clients.Client(connectionId).SendAsync("PopulatePlayers", obj);
     }
 }
